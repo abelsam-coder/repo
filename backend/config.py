45e28191c3a915b220601b87059e4b9bbf646f81
abelsam-coder <abelsamuel841@gmail.com>
2026-02-22 00:43:05 -0800

    front and back

---
+++ backend/config.py
@@ -0,0 +1,5 @@
+class Config():
+       # SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:@localhost/it'
+       SQLALCHEMY_DATABASE_URI='sqlite:///it.db'
+       UPLOAD_FOLDER='../upload'
+       SECRET_KEY='12345abe12345'
